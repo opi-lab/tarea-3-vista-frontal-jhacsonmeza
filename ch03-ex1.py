ae5e11c00832f02e09d7dc70938370d9fd19af02
jhacsonmeza <jhacsonmeza121@gmail.com>
2018-05-14 12:47:17 -0500

    Initial commit for ch03-ex1.py
    This code is able to calculate the homography between two images and map
    the transformed image

--- ch03-ex1.py
+++ ch03-ex1.py
@@ -1,55 +1,97 @@
+from matplotlib import pyplot as plt
 from PIL import Image
-import numpy
-import sys
+import numpy as np
+import os
 
 def normalize(points):
-  # Normalize homogenous points
-  for row in points:
-    row /= points[-1]
-  return points
+      """ Normalize a collection of points in
+      homogeneous coordinates so that last row = 1. """
+      
+      for row in points:
+            row /= points[-1]
+      
+      return points
+
 
 def make_homog(points):
-  # Make homogenous
-  return numpy.vstack((points, numpy.ones((1, points.shape[1]))))
+      """ Convert a set of points (dim*n array) to
+      homogeneous coordinates. """
+      
+      return np.vstack((points,np.ones((1,points.shape[1]))))
+
 
 def Htransform(im, H, out_size):
   # Applies a homography transform to im
   pil_im = Image.fromarray(im)
   pil_size = out_size[1], out_size[0]
-  return numpy.array(pil_im.transform(
+  return np.array(pil_im.transform(
     pil_size, Image.PERSPECTIVE, H.reshape(9)[0:8] / H[2,2], Image.LINEAR))
 
-def H_from_points(fp, tp):
-  # Find H such that H * fp = tp.
-  # 
-  # H has eight degrees of freedom, 
-  # so this needs at least 4 points in fp and tp.
-  
-  if fp.shape != tp.shape:
-    raise RuntimeError('number of points do not match')
-    #
-    # COMPLETE THIS FUNCTION!!
-    #
-    return H / H[2, 2]
+
+def H_from_points(x1, x2):
+      n = len(x1.T)
+      
+      # condition points (important for numerical reasons)
+      # --from points--
+      m = np.mean(x1[:2], axis=1)
+      maxstd = max(np.std(x1[:2], axis=1)) + 1e-9
+      C1 = np.diag([1/maxstd, 1/maxstd, 1])
+      C1[0][2] = -m[0]/maxstd
+      C1[1][2] = -m[1]/maxstd
+      x1 = np.dot(C1,x1)
+      # --to points--
+      m = np.mean(x2[:2], axis=1)
+      maxstd = max(np.std(x2[:2], axis=1)) + 1e-9
+      C2 = np.diag([1/maxstd, 1/maxstd, 1])
+      C2[0][2] = -m[0]/maxstd
+      C2[1][2] = -m[1]/maxstd
+      x2 = np.dot(C2,x2)
+      
+      A = np.ones([3*n,9])
+      zero = np.array([0,0,0])
+      for i in range(n):            
+            x = x1[:,i]
+            A[3*i] = np.hstack([zero, -x2[2,i]*x, x2[1,i]*x])
+            A[3*i+1] = np.hstack([x2[2,i]*x, zero, -x2[0,i]*x])
+            A[3*i+2] = np.hstack([-x2[1,i]*x, x2[0,i]*x, zero])
+            
+      *_,Vh = np.linalg.svd(A)
+      x = Vh.T[:,-1]
+      H = x.reshape(3,3)
+      
+      # decondition
+      H = np.dot(np.linalg.inv(C2),np.dot(H,C1))
+      
+      # normalize and return
+      return H / H[2,2]
+
+      
+im2 = np.array(Image.open(os.path.abspath('data/book_frontal.jpg')).convert('L'))
+im1 = np.array(Image.open(os.path.abspath('data/book_perspective.jpg')).convert('L'))
+
+plt.figure('Imagen 1')
+plt.axis('off'), plt.imshow(im1,cmap='gray')
+x1 = np.array(plt.ginput(4,mouse_add=3,mouse_pop=1,mouse_stop=2)).T
+plt.close()
+
+plt.figure('Imagen 2')
+plt.axis('off'), plt.imshow(im2,cmap='gray')
+x2 = np.array(plt.ginput(4,mouse_add=3,mouse_pop=1,mouse_stop=2)).T
+plt.close()
 
 
-if len(sys.argv) != 2:
-  print 'usage: %prog image.jpeg'
-  sys.exit(1)
+plt.figure()
+plt.imshow(np.hstack([im1,im2]),cmap='gray')
+plt.plot(x1[0],x1[1],'r.',markersize=6)
+plt.plot(x2[0]+im1.shape[1],x2[1],'r.',markersize=6)
+plt.axis('off'), plt.show()
 
-imname = sys.argv[1]
-im = array(Image.open(imname))
-imshow(im)
-gray()
-corners = array(ginput(4))
-# FIXME: sort corners, currently expects tl, bl, br, tr order.
-#
-# COMPLETE PROGRAM!!
-#
-# 
-mapped = Htransform(im, H, (h, w))
 
-figure()
-imshow(mapped)
-gray()
-show()
+xh1 = make_homog(x1)
+xh2 = make_homog(x2)
+H = H_from_points(xh1, xh2)
+im_out = Htransform(im2, H, im1.shape[:2])
+
+plt.figure()
+plt.imshow(im_out,cmap='gray')
+plt.axis('off'), plt.show()
